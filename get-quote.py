a977320100a97ddf2ab986fdf0e5d7d76085afe5
ValleyKnight <ValleyKnight@protonmail.com>
2021-01-05 10:20:41 -0500

    added another quote, print 2 quotes, remove newlines from ends of quotes, add shebang

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+
+## Imports
 import random
 
+## Do stuff
 def master():
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  f = open("quotes.txt") # Open quotes file
+  quotes = f.readlines() # Read the lines in variable f
+  f.close() # Close the file
 
-  rnd = random.randint(0, 13)
-  print(quotes[rnd])
+  print(quotes[random.randint(0, 13)].replace("\n", "")) # Print random quote, remove newline from end of quote
+  print(quotes[random.randint(0, 13)].replace("\n", "")) # Print random quote, remove newline from end of quote
 
 if __name__== "__main__":
   master()
